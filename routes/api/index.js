e2ac810579aba6bb08824cfd73e3e1a43c4bcacd
G Dog <greg.hanstedt@med.ge.com>
2021-03-03 17:07:19 -0500

    Added GET route /api/change-major that will only return changes where sys_tag = "MAJOR".

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const changeRoutes = require("./change");
+const changeRoutesMajor = require("./change-major");
 
 // Change routes
 router.use("/change", changeRoutes);
+router.use("/change-major", changeRoutesMajor);
 
 module.exports = router;
