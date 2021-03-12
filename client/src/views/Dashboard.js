b6df028238bcf026e34deedebe31f1ff7fe52b13
denisequarles1 <denise.quarles1@gmail.com>
2021-03-11 21:19:54 -0500

    updated dashboard with working month and partial working days

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -18,26 +18,6 @@ import {
   Tooltip,
 } from "react-bootstrap";
 
-var d = new Date();
-var testday= "Wednesday";
-var weekday = new Array(14);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-weekday[7] = "Sunday";
-weekday[8] = "Monday";
-weekday[9] = "Tuesday";
-weekday[10] = "Wednesday";
-weekday[11] = "Thursday";
-weekday[12] = "Friday";
-weekday[13] = "Saturday";
-
-var today = weekday[d.getDay()];
-var tomorrow = weekday[d.getDay()+1];
 
 
 class Dashboard extends Component {
@@ -50,6 +30,20 @@ class Dashboard extends Component {
     allChgsVolume: null,
     allChgsVolume2: null,
     allChgsVolume3: null,
+    sunAllChangeVolume: null,
+    sunMajorVolume: null,
+    monAllChangeVolume: null,
+    monMajorVolume: null,
+    tuesMajorVolume: null,
+    tuesAllChangeVolume: null,
+    wedMajorVolume: null,
+    wedAllChangeVolume: null,
+    thurMajorVolume: null,
+    thurAllChangeVolume: null,
+    friMajorVolume: null,
+    friAllChangeVolume: null,
+    satMajorVolume: null,
+    satAllChangeVolume: null,
 
     changes: [
       {
@@ -87,7 +81,7 @@ class Dashboard extends Component {
 //  How to use change-major to get month
 //    var request = axios.get("/api/change-bymonth-count?month=3&major=false&year=2021")
 
-
+    //Gets Normal changes for 6 days to create Dashboard values at the top of the page
     axios.get("/api/change-days-count?major=false&offset=0&days=6")
     .then ( request => {
       this.setState (
@@ -103,6 +97,7 @@ class Dashboard extends Component {
     );
 var changeVolume2;
 
+    //Gets Major changes for 6 days to create Dashboard values at the top of the page
     axios.get("/api/change-days-count?major=true&offset=0&days=6")
     .then ( request => {
       this.setState (
@@ -117,112 +112,242 @@ var changeVolume2;
     }
     );
     
-    axios.get("/api/change-days-count?major=false&offset=0&days=0")
-    .then ( request => {
-      this.setState (
-        {
-          changeVolume0: request.data
-        }
-      )
-    }
-    );
-
-    axios.get("/api/change-days-count?major=true&offset=0&days=0")
-    .then ( request => {
-      this.setState (
-        {
-          majorVolume0: request.data
-        }
-      )
-    }
-    );
-
-    axios.get("/api/change-days-count?major=false&offset=1&days=0")
-    .then ( request => {
-      this.setState (
-        {
-          changeVolume1: request.data
-        }
-      )
-    }
-    );
-
-    //Gets Major changes for March 2021
-    axios.get("/api/change-bymonth-count?month=3&major=true&year=2021")
-    .then ( request => {
-      this.setState (
-        {
-          monthMajorVolume: request.data
-        }
-      )
-    }
-    );
-
-    //Gets Major changes for April 2021
-    axios.get("/api/change-bymonth-count?month=4&major=true&year=2021")
-    .then ( request => {
-      this.setState (
-        {
-          monthMajorVolume2: request.data
-        }
-      )
-    }
-    );
+     //Gets Major changes for March 2021
+     axios.get("/api/change-bymonth-count?month=3&major=true&year=2021")
+     .then ( request => {
+       this.setState (
+         {
+           monthMajorVolume: request.data
+         }
+       )
+     }
+     );
  
-    //Gets Major changes for May 2021
-    axios.get("/api/change-bymonth-count?month=5&major=true&year=2021")
+     //Gets Major changes for April 2021
+     axios.get("/api/change-bymonth-count?month=4&major=true&year=2021")
+     .then ( request => {
+       this.setState (
+         {
+           monthMajorVolume2: request.data
+         }
+       )
+     }
+     );
+  
+     //Gets Major changes for May 2021
+     axios.get("/api/change-bymonth-count?month=5&major=true&year=2021")
+     .then ( request => {
+       this.setState (
+         {
+           monthMajorVolume3: request.data
+         }
+       )
+     }
+     );
+     
+     //Gets All changes for March 2021
+     axios.get("/api/change-bymonth-count?month=3&major=false&year=2021")
+     .then ( request => {
+       this.setState (
+         {
+           allChgsVolume: request.data
+         }
+       )
+     }
+     );
+ 
+     //Gets All changes for April 2021
+     axios.get("/api/change-bymonth-count?month=4&major=false&year=2021")
+     .then ( request => {
+       this.setState (
+         {
+           allChgsVolume2: request.data
+         }
+       )
+     }
+     );
+  
+     //Gets All changes for May 2021
+     axios.get("/api/change-bymonth-count?month=5&major=false&year=2021")
+     .then ( request => {
+       this.setState (
+         {
+           allChgsVolume3: request.data
+         }
+       )
+     }
+     );
+
+    //Gets All changes for Sun
+    axios.get("/api/change-days-count?major=false&offset=0&days=1")
     .then ( request => {
       this.setState (
         {
-          monthMajorVolume3: request.data
+          sunChangeVolume: request.data
+        }
+      )
+    }
+    );
+
+    //Gets Major changes for Sun
+    axios.get("/api/change-days-count?major=true&offset=0&days=1")
+    .then ( request => {
+      this.setState (
+        {
+          sunMajorVolume: request.data
+        }
+      )
+    }
+    );
+
+    //Gets All changes for Mon
+    axios.get("/api/change-days-count?major=false&offset=1&days=1")
+    .then ( request => {
+      this.setState (
+       {
+          monChangeVolume: request.data
+        }
+      )
+        }
+        );
+
+    //Gets Major changes for Mon
+    axios.get("/api/change-days-count?major=true&offset=1&days=1")
+    .then ( request => {
+      this.setState (
+        {
+          monMajorVolume: request.data
+        }
+      )
+    }
+    );
+
+    //Gets All changes for Tues
+    axios.get("/api/change-days-count?major=false&offset=2&days=1")
+    .then ( request => {
+      this.setState (
+        {
+          tuesAllChangeVolume: request.data
+        }
+      )
+    }
+    );
+
+    //Gets Major changes for Tues
+    axios.get("/api/change-days-count?major=true&offset=2&days=1")
+    .then ( request => {
+      this.setState (
+        {
+          tuesMajorVolume: request.data
+        }
+      )
+    }
+    );
+
+    //Gets All changes for Wed 
+    axios.get("/api/change-days-count?major=false&offset=3&days=1")
+    .then ( request => {
+      this.setState (
+        {
+          wedAllChangeVolume: request.data
+        }
+      )
+    }
+    );
+
+    //Gets Major changes for Wed 
+    axios.get("/api/change-days-count?major=true&offset=3&days=1")
+    .then ( request => {
+      this.setState (
+        {
+          wedMajorVolume: request.data
         }
       )
     }
     );
     
-    //Gets All changes for March 2021
-    axios.get("/api/change-bymonth-count?month=3&major=false&year=2021")
-    .then ( request => {
-      this.setState (
-        {
-          allChgsVolume: request.data
-        }
-      )
-    }
-    );
+ //Gets All changes for Thurs 
+ axios.get("/api/change-days-count?major=false&offset=4&days=1")
+ .then ( request => {
+   this.setState (
+     {
+       thursAllChangeVolume: request.data
+     }
+   )
+ }
+ );
 
-    //Gets All changes for April 2021
-    axios.get("/api/change-bymonth-count?month=4&major=false&year=2021")
-    .then ( request => {
-      this.setState (
-        {
-          allChgsVolume2: request.data
-        }
-      )
-    }
-    );
- 
-    //Gets All changes for May 2021
-    axios.get("/api/change-bymonth-count?month=5&major=false&year=2021")
-    .then ( request => {
-      this.setState (
-        {
-          allChgsVolume3: request.data
-        }
-      )
-    }
-    );
+ //Gets Major changes for Thurs 
+ axios.get("/api/change-days-count?major=true&offset=4&days=1")
+ .then ( request => {
+   this.setState (
+     {
+       thursMajorVolume: request.data
+     }
+   )
+ }
+ );
+   
+ //Gets All changes for Fri 
+ axios.get("/api/change-days-count?major=false&offset=5&days=1")
+ .then ( request => {
+   this.setState (
+     {
+       friAllChangeVolume: request.data
+     }
+   )
+ }
+ );
 
+ //Gets Major changes for Fri 
+ axios.get("/api/change-days-count?major=true&offset=5&days=1")
+ .then ( request => {
+   this.setState (
+     {
+       friMajorVolume: request.data
+     }
+   )
+ }
+ );
+
+ //Gets All changes for Sat 
+ axios.get("/api/change-days-count?major=false&offset=6&days=1")
+ .then ( request => {
+   this.setState (
+     {
+       satAllChangeVolume: request.data
+     }
+   )
+ }
+ );
+
+ //Gets Major changes for Sat 
+ axios.get("/api/change-days-count?major=true&offset=6&days=1")
+ .then ( request => {
+   this.setState (
+     {
+       satMajorVolume: request.data
+     }
+   )
+ }
+ );
 };  
   
-
-
   render () {
+    //Sets variable for normal1, normal2, and normal3 to get Normal changes for 3 montsh (Month Graph)
     var normal = this.state.allChgsVolume - this.state.monthMajorVolume
     var normal2 = this.state.allChgsVolume2 - this.state.monthMajorVolume2
     var normal3 = this.state.allChgsVolume3 - this.state.monthMajorVolume3
 
-    
+   
+     //Sets variable for normal4- normal10 to get Normal changes for Sunday- Saturday (Days Graph)
+     var normal4 = this.state.sunChangeVolume - this.state.sunMajorVolume3
+     var normal5 = this.state.monChangeVolume - this.state.monMajorVolume
+     var normal6 = this.state.tuesChangeVolume - this.state.tuesMajorVolume2
+     var normal7 = this.state.wedChangeVolume - this.state.wedMajorVolume3
+     var normal8 = this.state.thurChangeVolume - this.state.thurMajorVolume
+     var normal9 = this.state.friChangeVolume - this.state.friMajorVolume2
+     var normal10 = this.state.satChangeVolume - this.state.satMajorVolume3
 
     return (
       <>
@@ -294,7 +419,7 @@ var changeVolume2;
               <Card>
                 <Card.Header>
                   <Card.Title as="h4">2021 Changes by Month</Card.Title>
-                  <p className="card-category">(Major, and Normal)</p>
+                  <p className="card-category">(Total, Major, and Normal)</p>
                 </Card.Header>
                 <Card.Body>
                   <div className="ct-chart" id="chartHours">
@@ -306,10 +431,9 @@ var changeVolume2;
                           "May",
                         ],
                         series: [
+                          [this.state.allChgsVolume, this.state.allChgsVolume2, this.state.allChgsVolume3],
                           [normal, normal2, normal3],
-                          [this.state.monthMajorVolume, this.state.monthMajorVolume2, this.state.monthMajorVolume3]
-                          
-
+                          [this.state.monthMajorVolume, this.state.monthMajorVolume2, this.state.monthMajorVolume3]                         
                         ],
                       }}
                       type="Line"
@@ -346,10 +470,11 @@ var changeVolume2;
                 </Card.Body>
                 <Card.Footer>
                   <div className="legend">
-                    <i className="fas fa-circle text-info"></i>
-                    Total Changes <i className="fas fa-circle text-danger"></i>
-                    Major Changes<i className="fas fa-circle text-warning"></i>
-                    Normal Changes
+                    <i className="fas fa-circle text-info"></i> Total Changes
+                    <i className="fas fa-circle text-danger"></i> Major Changes 
+                    <i className="fas fa-circle text-warning"></i> Normal Changes  
+                    
+                   
                   </div>
                  
                 </Card.Footer>
@@ -380,15 +505,16 @@ var changeVolume2;
                         ], 
                         
                         series: [
-                          [],
-                        
+                          [this.state.monAllChangeVolume, this.state.tuesAllChangeVolume, this.state.wedAllChangeVolume, this.state.thurAllChangeVolume,this.state.friAllChangeVolume,this.state.satAllChangeVolume],
+                          [normal4, normal5, normal6, normal7, normal8, normal9, normal10],
+                          [this.state.sunMajorVolume, this.state.monMajorVolume, this.state.tuesMajorVolume2, this.state.wedMajorVolume3, this.state.thurMajorVolume, this.state.friMajorVolume, this.state.satMajorVolume] 
                         ],
                       }}
-                      type="Line"
+                      type="Bar"
                       
                       options={{
                         low: 0,
-                        high: 800,
+                        high: 100,
                         showArea: false,
                         height: "245px",
                         axisX: {
@@ -419,10 +545,9 @@ var changeVolume2;
                 </Card.Body>
                 <Card.Footer>
                   <div className="legend">
-                    <i className="fas fa-circle text-info"></i>
-                    Total Changes <i className="fas fa-circle text-danger"></i>
-                    Major Changes <i className="fas fa-circle text-warning"></i>
-                    Normal Changes 
+                    <i className="fas fa-circle text-info"></i> Total Changes
+                     <i className="fas fa-circle text-danger"></i> Major Changes 
+                    <i className="fas fa-circle text-warning"></i> Normal Changes                     
                   </div>
                  
                 </Card.Footer>
