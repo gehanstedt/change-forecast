32064ab4bb5621ef5331b5d4244b546a44a4fc30
Jerron <jerron.mcginnis@gmail.com>
2021-03-16 10:17:07 -0400

    Added classes for CSS to recreate login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import '../assets/css/login.css'
+import '../assets/css/light-bootstrap-dashboard-react.css'
+import logo from '../assets/img/logo1.png'; // with import
+
+
 
 // import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_disabled_web.png'
 // import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_normal_web.png'
@@ -39,28 +43,37 @@ class LoginForm extends Component {
 		} else {
 			return (
 				<body class="colorchange">
-				<div className="LoginForm">
-					<h1>LOGIN</h1>
-					<form>
-						<label htmlFor="username" class="loginForm">Username: </label>
-						<input
-							type="text"
-							name="username"
-							value={this.state.username}
-							onChange={this.handleChange}
-						/>
-						<br></br>
-						<label htmlFor="password" class="loginForm">Password: </label>
-						<input
-							type="password"
-							name="password"
-							value={this.state.password}
-							onChange={this.handleChange}
-						/>
-						<br></br>
-						<button class="btn" onClick={this.handleSubmit}>Login</button>
-					</form>
-				</div>
+					<div class="form-v8-content">
+						<div class="form-left">
+							<div class="img-fluid logoimg">
+								<img src={logo} />
+							</div>
+						</div>
+					<div class="form-right">
+						<div className="LoginForm">
+							<h1 class="login-banner">LOGIN</h1>
+							<form class="form-detail">
+								<label htmlFor="username" class="loginForm">Username: </label>
+								<input
+									type="text"
+									name="username"
+									value={this.state.username}
+									onChange={this.handleChange}
+								/>
+								<br></br>
+								<label htmlFor="password" class="loginForm">Password: </label>
+								<input
+									type="password"
+									name="password"
+									value={this.state.password}
+									onChange={this.handleChange}
+								/>
+								<br></br>
+								<button class="loginbtn" onClick={this.handleSubmit}>Login</button>
+							</form>
+						</div>
+					</div>
+					</div>
 				</body>
 
 			)
